eceb1f9bdcd16f1135b24618cc83433e903f0458
alzheimeer <fogniebla@hotmail.com>
2019-09-25 19:07:22 -0700

    feature new files

---
+++ t11-15.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+/**
+ * pchar - prints the char at the top of the stack, followed by a new line.
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+
+}
+/**
+ * pstr - prints the string starting at the top of the stack,
+ * followed by a new line.
+ * @stack: **pointer head of the stack
+ * @line_number: number of the file
+ * Return: void
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+}
+
+/**
+ * rotl - rotates the stack to the top of the stack.
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+
+}
+/**
+ * rotr - rotates the stack to the bottom of the stack.
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+
+}
---
+++ t6-10.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+/**
+ * sub - subtracts the top element stack
+ * @stack: ** pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+
+/**
+ * div - divides the second top element stack
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * mul - multiplies the second top element stack
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+
+
+
+}
+
+
+/**
+ * mod - module of the second top element stack
+ * @stack: **pointer head of the stack
+ * @line_number: number line in file
+ * Return: void
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+
+
+}
