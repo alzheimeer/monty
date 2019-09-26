8cc824a380cd5da592f68dfa3b1eb84e74b13881
alzheimeer <fogniebla@hotmail.com>
2019-09-25 23:12:23 -0700

    fix ascii

--- t11-15.c
+++ t11-15.c
@@ -7,17 +7,18 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !(*stack))
+	if (stack == NULL || *stack == NULL)
 	{
 		printf("L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (!isascii((*stack)->n))
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		printf("L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
+
 }
 /**
  * pstr - prints the string starting at the top of the stack,
